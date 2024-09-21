09d7242b019928851f93200c640a998ceb640ac9
Vineeth Vijayan <vineeth030@gmail.com>
2024-09-21 12:18:06 +0530

    first commit

---
+++ src/main/java/com/flavient/App.java
@@ -0,0 +1,13 @@
+package com.flavient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/flavient/AppTest.java
@@ -0,0 +1,19 @@
+package com.flavient;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertTrue( true );
+    }
+}
